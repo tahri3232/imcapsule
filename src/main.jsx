4a3f1d9773296cdadccee779faca9c78b91deaf8
Tahir Kha <tahir@bitsbridge.in>
2024-03-13 13:11:21 +0530

    hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import ThemeContextProvider from "./Store/ThemeContextProvider.jsx";
 import ContextProvider from "./Store/ContextProvider.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeContextProvider>
-    <ContextProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ContextProvider>
+      <ContextProvider>
+        <HashRouter>
+          <App />
+        </HashRouter>
+      </ContextProvider>
     </ThemeContextProvider>
   </React.StrictMode>
 );
